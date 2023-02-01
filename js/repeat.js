add165bef1c3a4ecaf34eb4ba96f53cbba417fab
Zuhriddin <zuhriddinabdulaxatov2005@gmail.com>
2023-02-01 18:53:54 +0500

    My First Commit Added

---
+++ js/repeat.js
@@ -0,0 +1,123 @@
+// First lesson
+// JavaScript connect
+
+
+
+// Second lesson
+// O'zgaruvchilar
+
+// Old version:
+// var myName = "Zuhriddin"
+// console.log(myName);
+
+// New version:
+// let friendName = "Nuriddin"
+// console.log(friendName);
+
+// const clientName = "Husniddin"
+// console.log(clientName);
+
+// friendName = "Alisher"
+// console.log(friendName);
+
+// clientName = "Mirjalol";
+// console.log(clientName);
+
+
+
+// Third lesson
+"use stric";
+
+
+
+// Fourst lesson
+// Data types
+
+// primitive:
+
+// Number
+// let Number = 55;
+// console.log(Number);
+// String
+// let myName = "Zuhriddin";
+// console.log(myName);
+// Boolean
+// let isMarried = false
+// console.log(isMarried);
+// Null
+// console.log(what);
+// Undefined
+// let Undef 
+// console.log(Undef);
+
+
+// object
+
+// object
+// const person = {
+//     herName: "Fahriddin",
+//     Age: 16,
+//     isMarried: false
+// }
+// console.log(person);
+
+// const friend = [
+//     'Nuriddin', 'Husniddin', 'Nurbek', [], {}
+// ]
+// console.log(friend);
+
+
+// alert("Hello world!");
+
+// const place = confirm("Are you here")
+// console.log(place);
+
+// const currentPlace = prompt("Where are you?", "Tashkent ");
+// console.log(currentPlace);
+
+// const age = +prompt("How old are you?")
+// console.log(typeof age);
+
+// const favouriteColor = [];
+
+// favouriteColor[0] = prompt("What's your favourite color #1")
+// favouriteColor[1] = prompt("What's your favourite color #2")
+// favouriteColor[2] = prompt("What's your favourite color #3")
+
+// console.log(typeof favouriteColor);
+
+
+
+// Fifth lesson
+
+// let channel = prompt("Eng yaxshi yuotuber kim? Uning foydalanuvchi nomini yozing...");
+// console.log(`www.youtube.com/${channel}`);
+
+// let user = "Zuhriddin";
+// let job = "Full-Stack developer"
+// console.log(`User name is ${user}, he is ${job}`);
+
+
+
+// Sixth lesson
+
+// let x = 12;
+// let y = "45";
+// console.log(x + y)
+
+// let incr = 10;
+// let decr = 10;
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(incr);
+// console.log(decr);
+
+// console.log(4 % 3);
+
+// console.log(4*4 === "16");
+
+// let first = false;
+// let second = false;
+// let third = true
+// console.log(first || second || third);
---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use stric";
+// console.log(6 - "4");
+
+// let incr = 10;
+// let decr = 10;
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(incr);
+// console.log(decr);
+
+// console.log(11 % 3);
+
+// console.log(5*6 === "30");
+
+// && --> va ; || --> yoki
+
+// let fAge = true;
+// let isClose = false;
+// console.log(fAge && !isClose);
+// console.log(fAge || isClose);
